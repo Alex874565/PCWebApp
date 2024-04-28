a9424b111b7c8a93e3b78ec0b9f26ea1f14f1e44
Letiția-Marta Iliescu <133118806+letitia-marta@users.noreply.github.com>
2024-04-28 19:41:42 +0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,20 +3,31 @@ import { createRoot } from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./index.css";
 import Home from "./pages/home/home";
+import Contact from "./pages/contact/contact";
+import About from "./pages/about/about";
+import Services from "./pages/services/services";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Home />,
-  },
-  {
-    path: "about",
-    element: <div>About Frontend</div>,
-  },
+const router = createBrowserRouter ([
+    {
+        path: "/",
+        element: <Home/>,
+    },
+    {
+        path: "/contact",
+        element: <Contact/>,
+    },
+    {
+        path: "/about",
+        element: <About/>,
+    },
+    {
+        path: "/services",
+        element: <Services/>,
+    },
 ]);
 
-createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>
+createRoot(document.getElementById("root")).render (
+    <React.StrictMode>
+        <RouterProvider router = {router} />
+    </React.StrictMode>
 );
