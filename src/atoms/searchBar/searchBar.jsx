e82cc77e521d89774a88c43c6b9ed8a9837819cb
Alex874565 <alex.carpinisan@gmail.com>
2024-05-16 00:03:50 +0300

    Added search bar functionality

--- src/atoms/searchBar/searchBar.jsx
+++ src/atoms/searchBar/searchBar.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import { useState } from 'react';
 
 const SearchBar = () => {
+  const [ keyword, setKeyword ] = useState("");
   return (
     <div className="search-bar">
-      <input type="text" placeholder="Search games" />
-      <button>Search</button>
+      <input onKeyUp={(e) => {if(e.keyCode == 13){window.location.href = `/products/${keyword}`;}}} onChange={(e) => {setKeyword(e.target.value)}} type="text" placeholder="Search games" />
+      <button onClick={() => (window.location.href = `/products/${keyword}`)}>Search</button>
     </div>
   );
 }
