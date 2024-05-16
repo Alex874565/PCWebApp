634f51b364b72aca00d99cd15b1b83c56de49a06
Letiția-Marta Iliescu <133118806+letitia-marta@users.noreply.github.com>
2024-05-17 00:30:28 +0300

    dashboard stats

--- src/pages/dashboard/dashboard.jsx
+++ src/pages/dashboard/dashboard.jsx
@@ -1,27 +1,52 @@
-import React from "react";
 import "./dashboard.css";
+import { useState, useEffect } from 'react';
 import { Link } from "react-router-dom";
+import axios from 'axios';
 
 const Dashboard = () => {
-    useEffect(
-        () => {if(localStorage.getItem('token')){axios.defaults.headers.common['Authorization'] = "Bearer " + JSON.parse(localStorage.getItem("token"))}}, 
-        []
-    )
+    const [users, setUsers] = useState([]);
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+        const fetchUsers = async () => {
+            try {
+                const res = await axios.get("http://localhost:3001/api/users/");
+                setUsers(res.data);
+            } catch (error) {
+                console.error('Error fetching users:', error);
+                setError('Failed to fetch users. Please try again later.');
+            }
+        };
+
+        fetchUsers();
+    }, []);
 
     return (
         <div>
-            <div className = "container">
-                <div className = "sidebar">
+            <div className="container">
+                <div className="sidebar">
                     <ul>
                         <li id = "active"><Link to = "/dashboard">Dashboard</Link></li>
-                        <li><Link to = "/users">Users</Link></li>
-                        <li><Link to = "/products">Products</Link></li>
-                        <li><Link to = "/orders">Orders</Link></li>
+                        <li><Link to = "/users_dashboard">Users</Link></li>
+                        <li><Link to = "/products_dashboard">Products</Link></li>
+                        <li><Link to = "/orders_dashboard">Orders</Link></li>
                         <li id = "red"><Link to = "/">Back to site</Link></li>
+                        <li id = "website-name">Love4Games</li>
                     </ul>
                 </div>
                 <div className = "main-content">
                     <h1>Admin dashboard</h1>
+                    
+                    <div className = "stats-container">
+                        <div className = "stat-card">
+                            <h2>Users</h2>
+                            <p>{users.length}</p>
+                        </div>
+                        <div className = "stat-card">
+                            <h2>Orders</h2>
+                            <p>0</p>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
