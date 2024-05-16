8df3f9be7da92b266c1836560cb35a6b42212791
Alex874565 <alex.carpinisan@gmail.com>
2024-05-17 00:23:37 +0300

    Added Filter

--- src/pages/products/products.jsx
+++ src/pages/products/products.jsx
@@ -7,17 +7,29 @@ import AI from "../../atoms/AI/AI";
 import "./products.css"; // Import your CSS file for styling
 import { Link } from "react-router-dom";
 import Footer from "../../atoms/footer/footer";
+import { max } from "@tensorflow/tfjs";
 
 function Products() {
   const { keyword } = useParams();
   const [products, setProducts] = useState([]);
   const [panelOpen, setPanelOpen] = useState(false);
+  const [renderedProducts, setRenderedProducts] = useState([]);
+  const [producer, setProducer] = useState("");
+  const [genre, setGenre] = useState("")
+  const [minPrice, setMinPrice] = useState(0)
+  const [maxPrice, setMaxPrice] = useState(0)
 
   function getProducts() {
     if (keyword == undefined) {
-      axios.get(`http://localhost:3001/api/products/keyword/`).then((resp) => setProducts(resp.data));
+      axios.get(`http://localhost:3001/api/products/keyword/`).then((resp) => {
+        setRenderedProducts(resp.data);
+        setProducts(resp.data);
+    });
     } else {
-      axios.get(`http://localhost:3001/api/products/keyword/${keyword}`).then((resp) => setProducts(resp.data));
+      axios.get(`http://localhost:3001/api/products/keyword/${keyword}`).then((resp) => {
+        setRenderedProducts(resp.data);
+        setProducts(resp.data);
+    });
     }
   }
 
@@ -25,20 +37,65 @@ function Products() {
     getProducts();
   }, []);
 
+
   const togglePanel = () => {
     setPanelOpen(!panelOpen);
   };
+
+  function filterProduct(product){
+    if((product.price >= minPrice || !minPrice) && (product.price <= maxPrice || maxPrice == 0 || !maxPrice)){
+        if(product.genre == genre || genre == "" || !genre){
+            if(product.producer == producer || producer == "" || !producer){
+                return product
+            }
+        }
+    }
+  }
+
+  function resetRenderedProducts(){
+    setRenderedProducts(products);
+  }
+
+  function filterProducts(){
+    setRenderedProducts(products.filter(product => filterProduct(product)))
+  }
+
+ function renderFilterPanel(){
+    return (<div className={`sidepanel ${panelOpen ? 'open' : 'closed'}`}>
+        <button className="closeBtn" onClick={togglePanel}>Close</button>
+        <div className="filterGroup">
+          <label htmlFor="producer">Producer:</label>
+          <input onChange={(e) => setProducer(e.target.value)} type="text" id="producer" name="producer" />
+        </div>
+        <div className="filterGroup">
+          <label htmlFor="min_price">MinPrice:</label>
+          <input onChange={(e) => setMinPrice(parseFloat(e.target.value))} type="number" id="min_price" name="min_price" />
+        </div>
+        <div className="filterGroup">
+          <label htmlFor="max_price">MaxPrice:</label>
+          <input onChange={(e) => setMaxPrice(parseFloat(e.target.value))} type="number" id="max_price" name="max_price" />
+        </div>
+        <div className="filterGroup">
+          <label htmlFor="genre">Genre:</label>
+          <input onChange={(e) => setGenre(e.target.value)} type="text" id="genre" name="genre" />
+        </div>
+        <button className="applyBtn" onClick={filterProducts}>Apply</button>
+        <button className="resetBtn" onClick={resetRenderedProducts}>Reset</button>
+      </div>
+    );
+ }
 
   return (
     <div>
       <Navbar />
       {!panelOpen && <button className="openFilterBtn" onClick={togglePanel}>Open Filter</button>}
-      <FilterPanel isOpen={panelOpen} onClose={togglePanel} />
-      {!products[0] && <p>No products matching that keyword!</p>}
+      {renderFilterPanel()}
+      {console.log(renderedProducts)}
+      {!renderedProducts[0] && <p>No products matching that filter/keyword!</p>}
       <h1>List of Products:</h1>
         <ul className="products-container">
-          {products.map((product) => (
-            <li key={product.id} className="product-item"> {/* Added className */}
+          {renderedProducts.map((product) => (
+            <li key={product._id} className="product-item"> {/* Added className */}
               <Link to={`/product/${product._id}`} className="link">
                 {/* Wrap product with Link */}
                 <img className="product-image" src={product.image} alt={product.name} />
