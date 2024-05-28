de8c0c57a7fb126dbed9335928395b42d1d324d8
Letiția-Marta Iliescu <133118806+letitia-marta@users.noreply.github.com>
2024-05-28 22:10:01 +0300

    moved AI assistant to the right

--- src/atoms/AI/AI.jsx
+++ src/atoms/AI/AI.jsx
@@ -3,99 +3,110 @@ import './AI.css';
 import '@chatscope/chat-ui-kit-styles/dist/default/styles.min.css';
 import { MainContainer, ChatContainer, MessageList, Message, MessageInput, TypingIndicator } from '@chatscope/chat-ui-kit-react';
 import axios from 'axios';
-import { useParams } from 'react-router-dom';
 
-function AI(products) {
-  const [messages, setMessages] = useState([
-    {
-      message: "Hi, I am your AI assistant. How can I help you?",
-      sentTime: "just now",
-      sender: "AI",
-      direction: "incoming"
+function AI() {
+    const [messages, setMessages] = useState([
+        {
+            message: "Hi, I am your AI assistant. How can I help you?",
+            sentTime: "just now",
+            sender: "AI",
+            direction: "incoming"
+        }
+    ]);
+    const [isTyping, setIsTyping] = useState(false);
+    const [products, setProducts] = useState(null);
+    const [isMinimized, setIsMinimized] = useState(true);
+
+    const handleSend = async (message) => {
+        const newMessage = {
+            message,
+            direction: 'outgoing',
+            sender: 'user'
+        };
+
+        const newMessages = [...messages, newMessage];
+        
+        setMessages(newMessages);
+
+        setIsTyping(true);
+        await processMessageToAI(newMessages);
+    };
+
+    function getProducts() {
+        axios
+            .get(`http://localhost:3001/api/products/`)
+            .then((resp) => {setProducts(resp.data)})
     }
-  ]);
-  const [isTyping, setIsTyping] = useState(false);
-  const [isMinimized, setIsMinimized] = useState(true);
 
-  const handleSend = async (message) => {
-    const newMessage = {
-      message,
-      direction: 'outgoing',
-      sender: 'user'
-    };
-
-    const newMessages = [...messages, newMessage];
-    
-    setMessages(newMessages);
-
-    setIsTyping(true);
-    await processMessageToAI(newMessages);
-  };
+    useEffect(() => {getProducts(); console.log("Getting products...")}, [])
 
 
+    async function processMessageToAI (chatMessages) {
+        let apiMessages = chatMessages.map((messageObject) => {
+            let role = "";
+            if (messageObject.sender === "AI")
+                role = "assistant";
+            else
+                role = "user";
+            return { role: role, content: messageObject.message }
+        });
 
-  async function processMessageToAI(chatMessages) {
-    let apiMessages = chatMessages.map((messageObject) => {
-      let role = "";
-      if (messageObject.sender === "AI") {
-        role = "assistant";
-      } else {
-        role = "user";
-      }
-      return { role: role, content: messageObject.message}
-    });
+        function apiRequestBody (products) {
+            console.log(products)
+            return ({
+                "model": "gpt-3.5-turbo",
+                "messages": [
+                    {"role": "system", "content": `Give answers as a shopping assistant. Please give all recommendations exclusevly from the following JSON-formatted products:\n ${products.map((product) => {
+                        return `Name: ${product.name}, Description: ${product.description}, Price: ${product.price}, Genre: ${product.genre}, Producer: ${product.producer}, Release date: ${product.date}\n;`})}.`},
+                  ...apiMessages
+                ]
+            });
+        };
 
-    function apiRequestBody(products){
-        console.log(products)
-      return ({
-        "model": "gpt-3.5-turbo",
-        "messages": [
-            {"role": "system", "content": `Give answers as a shopping assistant. Please give all recommendations exclusively from the following JSON-formatted products:\n ${products.map((product) => {
-                return `Name: ${product.name}, Description: ${product.description}, Price: ${product.price}, Genre: ${product.genre}, Producer: ${product.producer}, Release date: ${product.date}\n;`})}.`},
-          ...apiMessages
-        ]
-      });
-    };
-    console.log("Fetching resp")
-    await fetch(import.meta.env.VITE_CRACK_API, 
-    {
-      method: "POST",
-      headers: {
-        "Authorization": "Bearer " + import.meta.env.VITE_CRACK_API_KEY,
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify(apiRequestBody(products))
-    }).then((data) => {
-      return data.json();
-    }).then((data) => {
-      setMessages([...chatMessages, {
-        message: data.choices[0].message.content,
-        direction: "incoming",
-        sender: "AI"
-      }])
-      setIsTyping(false);
-    });
-    console.log("Fetch successful!");
-}
+        console.log("Fetching resp")
+        await fetch(import.meta.env.VITE_CRACK_API, 
+        {
+            method: "POST",
+            headers: {
+              "Authorization": "Bearer " + import.meta.env.VITE_CRACK_API_KEY,
+              "Content-Type": "application/json"
+            },
+            body: JSON.stringify(apiRequestBody(products))
+        }).then((data) => {
+            return data.json();
+        }).then((data) => {
+            setMessages([...chatMessages, {
+                message: data.choices[0].message.content,
+                direction: "incoming",
+                sender: "AI"
+            }])
+            setIsTyping(false);
+        });
+        console.log("Fetch successful!");
+    }
 
-  return (
-    <div className="ai_body">
-    <h2>AI Assistant</h2>
-      <MainContainer>
-        <ChatContainer>       
-          <MessageList
-            scrollBehavior="smooth"
-            onScroll={(e) => e.stopPropagation()} 
-            typingIndicator={isTyping ? <TypingIndicator content="AI is typing" /> : null}
-          >
-            {messages.map((message, i) => {
-              return <Message key={i} model={message}/>
-            })}
-          </MessageList>
-          <MessageInput placeholder="Type message here" onSend={handleSend} />        
-        </ChatContainer>
-      </MainContainer>
-    </div>
-  )
+    return (
+        <div className = "ai_body">
+            <div className = "assistant-header">
+                <span className = "assistant-text">AI Assistant</span>
+                <button id = "minimize_button" onClick = {() => setIsMinimized(!isMinimized)}>-</button>
+            </div>
+            { !isMinimized &&
+                <MainContainer>
+                    <ChatContainer>       
+                        <MessageList
+                            scrollBehavior = "smooth" 
+                            typingIndicator = { isTyping ? <TypingIndicator content = "AI is typing" /> : null }
+                        >
+                            { messages.map((message, i) => {
+                                return <Message key = {i} model = {message}/>
+                            })}
+                        </MessageList>
+                        <MessageInput placeholder = "Type message here" onSend = {handleSend} />        
+                    </ChatContainer>
+                </MainContainer>
+            }
+        </div>
+    )
 }
 export default AI;
